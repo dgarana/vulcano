68ca72c853e9468f63eba42bc92169da0899d446
David Garaña <dgarana@live.com>
2018-11-18 00:44:39 +0100

    Create Vulcano App
    Create a vulcano app that can register commands into it in order to be
    able to execute them after through command line or REPL.
    
    This closes: #1

---
+++ vulcano/app/test_classes.py
@@ -0,0 +1,53 @@
+# -* coding: utf-8 *-
+# System imports
+import unittest
+
+# Third-party imports
+# Local imports
+from .classes import VulcanoApp
+
+
+class TestVulcanoApp(unittest.TestCase):
+    """
+    Test the Vulcano APP
+    """
+
+    def setUp(self):
+        self.vulcano_app = VulcanoApp()
+
+    def tearDown(self):
+        # Remove the singleton instances before continue next test
+        VulcanoApp._instance = None
+
+    def test_it_should_register_and_execute_commands_with_args(self):
+        """
+        Vulcano app should be able to register commands with normal arguments
+        """
+        def test_function(what):
+            return what
+
+        self.vulcano_app.register('test_function', test_function,
+                             'This is just a test function')
+        result = self.vulcano_app.run('test_function', 'Passed!')
+        self.assertEquals(result, 'Passed!')
+
+    def test_it_should_register_and_execute_commands_with_kwargs(self):
+        """
+        Vulcano app should be able to register commands with known arguments
+        """
+        def test_function(arg1=None, arg2=None):
+            return arg1
+
+        self.vulcano_app.register('test_function', test_function,
+                             'This is just a test function')
+        result = self.vulcano_app.run('test_function', arg2='No one',
+                                      arg1='Passed!')
+        self.assertEquals(result, 'Passed!')
+
+    def test_it_should_not_register_commands_with_same_name(self):
+        """
+        Vulcano app cannot register two commands with same name
+        """
+        self.vulcano_app.register('foo', lambda x: None, 'Dummy function')
+        with self.assertRaises(NameError):
+            self.vulcano_app.register('foo', lambda x: None, 'Dummy function')
---
+++ vulcano/core/classes.py
@@ -0,0 +1,21 @@
+# -* coding: utf-8 *-
+"""
+:py:mod:`vulcano.core.classes`
+------------------------------
+"""
+
+
+class Singleton(object):
+    """
+    Singleton Pattern class.
+
+    Following the singleton pattern, it will return or create an instance
+    depending if it already exists or not.
+    """
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(Singleton, cls).__new__(
+                                cls, *args, **kwargs)
+        return cls._instance
