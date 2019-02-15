b1e4bb648672e84492061c2aa92eb1b6ba4988dc
David Garaña <dgarana@users.noreply.github.com>
2019-02-15 18:11:48 +0100

    Improve tests argv names
    This will give more context to next user that comes in and take a look
    at it ;)

--- vulcano/app/test_classes.py
+++ vulcano/app/test_classes.py
@@ -7,7 +7,7 @@ class TestVulcanoApp(TestCase):
 
     @patch('vulcano.app.classes.sys')
     def test_should_register_builtin_functions_before_run_args(self, sys_mock):
-        sys_mock.argv = ['argument1', 'help']
+        sys_mock.argv = ['ensure_no_repl', 'help']
         app = VulcanoApp()
         app.run()
         self.assertIn('exit', app._manager._commands)
