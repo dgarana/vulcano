48062287d06ab106c8e7ce48198dcd6313e881d6
David Garaña <dgarana@users.noreply.github.com>
2019-02-15 18:11:24 +0100

    Improve README documentation
    Add new example and also a gif executing the example.

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -3,11 +3,15 @@ from vulcano.app.classes import VulcanoApp
 
 app = VulcanoApp()
 
-@app.register("another_command")
-def my_command(arg_1, agument_2="Hello"):
-    """ This is just The help """
-    print(arg_1)
-    print(agument_2)
+@app.register("hi", "Salute people given form parameter")
+def salute_method_here(name, title="Mr."):
+    print("Hi! {} {} :) Glad to see you.".format(title, name))
+
+
+@app.register()
+def bye(name="User"):
+    """ Say goodbye to someone """
+    print("Bye {}!".format(name))
 
 
 if __name__ == '__main__':
