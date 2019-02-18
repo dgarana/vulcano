1c6d3bce6407bcfd2e8c0818553acfed26160002
David Garaña <dgarana@live.com>
2019-02-19 00:40:59 +0100

    Remove upload script in favour of CI

--- pip_upload.sh
+++
@@ -1,7 +0,0 @@
-echo "1. Removing old distributables ..."
-rm dist/*
-echo "2. Creating python packages ..."
-python3 setup.py sdist bdist_wheel
-echo "3. Uploading to pip repository ..."
-python3 -m twine upload dist/*
-echo "Finished"
