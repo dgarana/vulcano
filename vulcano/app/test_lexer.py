dd368c21d83d13c1a82cc152d1921fd67415d9ab
David Garaña <dgarana@MacBook-Pro-de-David.local>
2019-03-06 19:33:02 +0100

    Apply black formatter

--- vulcano/app/test_lexer.py
+++ vulcano/app/test_lexer.py
@@ -3,11 +3,10 @@ from vulcano.app.lexer import create_lexer
 
 
 class TestCreateLexer(TestCase):
-
     def test_should_have_commands_inside(self):
-        commands = ['help', 'exit', 'command_1']
-        expected_regex = r'^(help|exit|command_1)\b'
+        commands = ["help", "exit", "command_1"]
+        expected_regex = r"^(help|exit|command_1)\b"
         lexer = create_lexer(commands)
-        root_tokens = lexer.tokens['root']
+        root_tokens = lexer.tokens["root"]
         command_tokens, _ = root_tokens[0]
         self.assertEquals(expected_regex, command_tokens)
