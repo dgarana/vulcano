a085edf2610832e0b8880171262b0dc50f9ef742
David Garaña <dgarana@MacBook-Pro-de-David.local>
2019-03-15 01:34:01 +0100

    Move from MD to RST (for sphinx)
    In order to keep consistency on the documenting format to use, we've
    moved away from MD to RST (just because sphinx is already using RST
    format to generate all the project documentation)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import vulcano
 HERE = path.abspath(path.dirname(__file__))
 
 
-with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
