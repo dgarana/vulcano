9584d302007836da1a9070ae288eb49e4e3e6867
David Garaña <dgarana@MacBook-Pro-de-David.local>
2019-05-11 01:26:39 +0200

    Display custom error when command not found
    Related with issue #78

---
+++ vulcano/exceptions.py
@@ -0,0 +1,19 @@
+# -* coding: utf-8 *-
+"""
+:py:mod:`vulcano.exceptions`
+----------------------------
+Exceptions raised by Vulcano
+"""
+# System imports
+# Third-party imports
+# Local imports
+
+
+class VulcanoException(Exception):
+    """Main vulcano exceptions"""
+    pass
+
+
+class CommandNotFound(VulcanoException):
+    """Raised when there's no command"""
+    pass
