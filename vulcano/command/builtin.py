4de12abdc841258577c7a70554e410d53387e790
David Garaña <dgarana@MacBook-Pro-de-David.local>
2019-05-11 00:19:50 +0200

    Get help for just a command
    Help builtin function now can return help only for a command given as
    parameter.

--- vulcano/command/builtin.py
+++ vulcano/command/builtin.py
@@ -5,6 +5,7 @@
 Builtin commands that comes by default with vulcano.
 """
 # System imports
+from __future__ import print_function
 import sys
 
 # Third-party imports
@@ -15,10 +16,17 @@ __all__ = ["help", "exit"]
 
 
 def help(app):
-    def real_help():
+    def real_help(command=None):
         """ Print this help """
-        for command in app.manager._commands.values():
-            print(command.help)
+        if command:
+            command = app.manager._commands.get(command)
+            if command:
+                print(command.help)
+            else:
+                print("Command `{}` not found".format(command))
+        else:
+            for command in app.manager._commands.values():
+                print(command.help)
 
     return real_help
 
